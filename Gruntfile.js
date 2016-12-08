8196dca71dedd351333218db093a261e5bee4c46
Michiel Gerritsen <michiel.gerritsen@tig.nl>
2016-12-08 09:34:10 +0100

    Moved tests to Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,16 @@
 module.exports = function(grunt) {
+    var phpunitPath = 'phpunit.xml';
+
+    if (grunt.file.isDir('/tmp/magento2/')) {
+        phpunitPath = '/tmp/magento2/vendor/tig/postnl/phpunit.xml.dist'
+    }
 
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         exec: {
             phpcs: 'php -ddisplay_errors=1 ~/.composer/vendor/bin/phpcs --standard=phpcs.xml --extensions=php .',
-            phpunit: 'phpunit'
+            phpunit: 'phpunit -c "' + phpunitPath + '"'
         }
     });
 
