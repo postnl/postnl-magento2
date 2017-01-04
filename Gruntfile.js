67f2d8e1378464cf5636d35fdf0fb32cd91e23ab
Michiel Gerritsen <michiel.gerritsen@tig.nl>
2017-01-04 10:00:41 +0100

    Added grunt option to calculate the code coverage

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function (grunt) {
     var magento2path = '../../../../';
     var phpunitXmlPath = __dirname + '/phpunit.xml';
+    var buildPath = __dirname + '/Build/';
 
     if (grunt.file.isDir('/tmp/magento2/')) {
         magento2path = '/tmp/magento2/';
@@ -17,15 +18,21 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         exec: {
-            phpcsEasy: phpcsCommand + '--severity=10 .',
-            phpcsFull: phpcsCommand + ' .',
+            phpcs_easy: phpcsCommand + '--severity=10 .',
+            phpcs_full: phpcsCommand + ' .',
             unitTests: 'cd ' + magento2path + ' && vendor/bin/phpunit -c "' + phpunitXmlPath + '"',
             integrationTests:
                 'cd ' + magento2path + 'dev/tests/integration &&' +
                 '../../../vendor/bin/phpunit --testsuite "TIG PostNL Integration Tests"',
             phplint: 'find . -name "*.php" ! -path "./vendor/*" -print0 | xargs -0 -n 1 -P 8 php -l',
             translations_nl: '../../../../bin/magento i18n:collect-phrases -vvv . -o i18n/nl_NL.csv',
-            translations_en: '../../../../bin/magento i18n:collect-phrases -vvv . -o i18n/en_US.csv'
+            translations_en: '../../../../bin/magento i18n:collect-phrases -vvv . -o i18n/en_US.csv',
+            code_coverage:
+                'mkdir -p ' + buildPath + '/coverage/{unit,integration} && ' +
+                'cd ' + magento2path + ' && ' +
+                'vendor/bin/phpunit -c "' + phpunitXmlPath + '" --coverage-html ' + buildPath + '/coverage/unit && ' +
+                'cd dev/tests/integration &&' +
+                '../../../vendor/bin/phpunit --testsuite "TIG PostNL Integration Tests"  --coverage-html ' + buildPath + '/coverage/integration'
         },
         jshint: {
             all: [
@@ -42,11 +49,12 @@ module.exports = function (grunt) {
     grunt.registerTask('test', ['jshint:all', 'exec:phplint', 'exec:phpunit', 'exec:phpcs']);
     grunt.registerTask('translations', ['exec:translations_nl', 'exec:translations_en']);
     grunt.registerTask('lint', ['exec:phplint', 'jshint:all']);
-    grunt.registerTask('phpcs', ['exec:phpcsFull']);
+    grunt.registerTask('phpcs', ['exec:phpcs_full']);
+    grunt.registerTask('code_coverage', ['exec:code_coverage']);
     grunt.registerTask('test', [
         'exec:unitTests',
         'exec:integrationTests',
-        'exec:phpcsEasy',
+        'exec:phpcs_easy',
         'exec:phplint',
         'jshint:all'
     ]);
