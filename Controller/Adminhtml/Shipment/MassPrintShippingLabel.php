646f47c14c868e1fa0de4bcba1ddcb42b7543563
Mickey Beijer <mickey.beijer@tig.nl>
2017-01-10 16:46:10 +0100

    PostNL shipment labels can now be confirmed and printed via Shipment Grid

--- Controller/Adminhtml/Shipment/MassPrintShippingLabel.php
+++ Controller/Adminhtml/Shipment/MassPrintShippingLabel.php
@@ -44,8 +44,12 @@ use Magento\Framework\App\Response\Http\FileFactory;
 use Magento\Framework\App\ResponseInterface;
 use Magento\Backend\App\Action\Context;
 use Magento\Sales\Model\Order\Shipment;
+use Magento\Shipping\Model\Shipping\LabelGenerator;
 use Magento\Ui\Component\MassAction\Filter;
 use Magento\Sales\Model\ResourceModel\Order\Shipment\CollectionFactory as ShipmentCollectionFactory;
+
+use TIG\PostNL\Model\ResourceModel\Shipment\CollectionFactory as PostnlShipmentCollectionFactory;
+use TIG\PostNL\Webservices\Endpoints\Labelling;
 
 class MassPrintShippingLabel extends Action
 {
@@ -53,6 +57,11 @@ class MassPrintShippingLabel extends Action
      * @var Shipment
      */
     private $currentShipment;
+
+    /**
+     * @var array
+     */
+    private $labels;
 
     /**
      * @var Filter
@@ -65,26 +74,50 @@ class MassPrintShippingLabel extends Action
     private $collectionFactory;
 
     /**
+     * @var PostnlShipmentCollectionFactory
+     */
+    private $postnlCollectionFactory;
+
+    /**
      * @var FileFactory
      */
     private $fileFactory;
 
     /**
-     * @param Context                   $context
-     * @param Filter                    $filter
-     * @param ShipmentCollectionFactory $collectionFactory
-     * @param FileFactory               $fileFactory
+     * @var LabelGenerator
+     */
+    private $labelGenerator;
+
+    /**
+     * @var Labelling
+     */
+    private $labelling;
+
+    /**
+     * @param Context                         $context
+     * @param Filter                          $filter
+     * @param ShipmentCollectionFactory       $collectionFactory
+     * @param PostnlShipmentCollectionFactory $postnlCollectionFactory
+     * @param FileFactory                     $fileFactory
+     * @param LabelGenerator                  $labelGenerator
+     * @param Labelling                       $labelling
      */
     public function __construct(
         Context $context,
         Filter $filter,
         ShipmentCollectionFactory $collectionFactory,
-        FileFactory $fileFactory
+        PostnlShipmentCollectionFactory $postnlCollectionFactory,
+        FileFactory $fileFactory,
+        LabelGenerator $labelGenerator,
+        Labelling $labelling
     ) {
         parent::__construct($context);
         $this->filter = $filter;
         $this->collectionFactory = $collectionFactory;
+        $this->postnlCollectionFactory = $postnlCollectionFactory;
         $this->fileFactory = $fileFactory;
+        $this->labelGenerator = $labelGenerator;
+        $this->labelling = $labelling;
     }
 
     /**
@@ -98,6 +131,23 @@ class MassPrintShippingLabel extends Action
         $collection = $this->collectionFactory->create();
         $collection = $this->filter->getCollection($collection);
 
+        /**
+         * TODO: Flow according to M1 version:
+         *
+         * 1. check if user is allowed to perform action > N/A
+         * 2. check maximum label printing > Unsure if necessary
+         * 3. set_time_limit(0) > Unsure if necessary
+         * 4. check if checked shipments are PostNL shipments (if not, addWarning()) > Partly done, no warning is shown
+         * 5. Confirm & get labels > Done
+         * 5.a Confirming -> just confirm, no labels > Unsure if necessary
+         * 5.b GenerateLabel -> both confirm and labels > Done
+         * 5.c GenerateLabelWithoutConfirm -> just label, no confirm. Confirming webservice has to be used after this. > todo
+         * 5.d in short, the api flow is either GenerateLabelWithoutConfirm -> Confirming or just GenerateLabel
+         * 6. Check warnings/errors > todo
+         * 7. Update shipment/order status > todo
+         * 8. Create & print PDFs > done
+         */
+
         /** @var Shipment $shipment */
         foreach ($collection as $shipment) {
             $this->currentShipment = $shipment;
@@ -109,17 +159,52 @@ class MassPrintShippingLabel extends Action
 
     private function getLabel()
     {
-        // @codingStandardsIgnoreStart
-        // @todo
+        $collection = $this->postnlCollectionFactory->create();
+        $collection->addFieldToFilter('shipment_id', array('eq' => $this->currentShipment->getId()));
+
+        //TODO: add a proper warning notifying of a non-postnl shipment
+        if (count($collection) < 0) {
+            return;
+        }
+
+        /** @var \TIG\PostNL\Model\Shipment $postnlShipment */
+        foreach ($collection as $postnlShipment) {
+            //TODO: catch the error and notify the user of it
+            $this->labels[] = $this->generateLabel($postnlShipment);
+        }
+
         return;
-        // @codingStandardsIgnoreEnd
     }
 
+    /**
+     * @param \TIG\PostNL\Model\Shipment $postnlShipment
+     *
+     * @return \Magento\Framework\Phrase
+     */
+    private function generateLabel($postnlShipment)
+    {
+        $this->labelling->setParameters($postnlShipment);
+        $response = $this->labelling->call();
+
+        if (!is_object($response) || !isset($response->ResponseShipments->ResponseShipment)) {
+            return __('Invalid generateLabel response: %1', var_export($response, true));
+        }
+
+        return $response->ResponseShipments->ResponseShipment[0]->Labels->Label[0]->Content;
+    }
+
+    /**
+     * @return ResponseInterface
+     * @throws \Exception
+     */
     private function outputPdf()
     {
+        /** @var \Zend_Pdf $combinedLabels */
+        $combinedLabels = $this->labelGenerator->combineLabelsPdf($this->labels)->render();
+
         return $this->fileFactory->create(
             'ShippingLabels.pdf',
-            'PDF OUTPUT',
+            $combinedLabels,
             DirectoryList::VAR_DIR,
             'application/pdf'
         );
