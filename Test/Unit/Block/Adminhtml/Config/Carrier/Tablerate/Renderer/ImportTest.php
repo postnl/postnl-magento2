e6df8a72fefda9a0a3ce18459674489b7d75ceec
Mickey Beijer <mickey.beijer@tig.nl>
2017-02-23 16:12:51 +0100

    Added some new Unittests

---
+++ Test/Unit/Block/Adminhtml/Config/Carrier/Tablerate/Renderer/ImportTest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ *
+ *          ..::..
+ *     ..::::::::::::..
+ *   ::'''''':''::'''''::
+ *   ::..  ..:  :  ....::
+ *   ::::  :::  :  :   ::
+ *   ::::  :::  :  ''' ::
+ *   ::::..:::..::.....::
+ *     ''::::::::::::''
+ *          ''::''
+ *
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@tig.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@tig.nl for more information.
+ *
+ * @copyright   Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+namespace TIG\PostNL\Test\Unit\Block\Adminhtml\Config\Carrier\Tablerate\Renderer;
+
+use TIG\PostNL\Block\Adminhtml\Config\Carrier\Tablerate\Renderer\Import;
+use TIG\PostNL\Test\TestCase;
+
+/**
+ * Class ImportTest
+ *
+ * @package TIG\PostNL\Test\Unit\Block\Adminhtml\Config\Carrier\Tablerate\Renderer
+ */
+class ImportTest extends TestCase
+{
+    protected $instanceClass = Import::class;
+
+    public function testGetTimeConditionName()
+    {
+        $conditionName = 'some_condition_name';
+
+        $instance = $this->getInstance();
+        $instance->setTimeConditionName($conditionName);
+
+        $result = $instance->getTimeConditionName();
+        $this->assertEquals($conditionName, $result);
+    }
+}
