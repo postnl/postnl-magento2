c0bd47da756e198bca61078adef25acd5796d1f0
Rik Jonkmans <rik@tig.nl>
2017-05-16 11:19:54 +0200

    fixed unittest

--- Observer/TIGPostNLShipmentSaveAfter/UpdateOrderGrid.php
+++ Observer/TIGPostNLShipmentSaveAfter/UpdateOrderGrid.php
@@ -88,10 +88,14 @@ class UpdateOrderGrid implements ObserverInterface
             return;
         }
 
-        $productCode = $shipment->getProductCode() !== null ? $shipment->getProductCode() : $order->getProductCode();
+        $productCode = $shipment->getProductCode() ?: $order->getProductCode();
 
-        $order->setData('ship_at', $shipment->getShipAt());
-        $order->setData('product_code', $productCode);
+        $data = [
+            'ship_at' => $shipment->getShipAt(),
+            'product_code' => $productCode
+        ];
+
+        $order->setData($data);
 
         $this->orderRepositoryInterface->save($order);
     }
