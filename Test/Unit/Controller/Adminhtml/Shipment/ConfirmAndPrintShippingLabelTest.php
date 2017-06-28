1ebcf09d771b3ef7926455350cd0bdebe3d41a89
Mickey Beijer <mickey.beijer@tig.nl>
2017-06-28 17:01:15 +0200

    Added unittest for admin shipment controller

---
+++ Test/Unit/Controller/Adminhtml/Shipment/ConfirmAndPrintShippingLabelTest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ *
+ *          ..::..
+ *     ..::::::::::::..
+ *   ::'''''':''::'''''::
+ *   ::..  ..:  :  ....::
+ *   ::::  :::  :  :   ::
+ *   ::::  :::  :  ''' ::
+ *   ::::..:::..::.....::
+ *     ''::::::::::::''
+ *          ''::''
+ *
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@tig.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@tig.nl for more information.
+ *
+ * @copyright   Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+namespace TIG\PostNL\Unit\Controller\Adminhtml\Shipment;
+
+use Magento\Sales\Model\Order\Shipment;
+use Magento\Sales\Model\Order\ShipmentRepository;
+use TIG\PostNL\Controller\Adminhtml\Shipment\ConfirmAndPrintShippingLabel;
+use TIG\PostNL\Helper\Tracking\Track;
+use TIG\PostNL\Test\TestCase;
+
+class ConfirmAndPrintShippingLabelTest extends TestCase
+{
+    protected $instanceClass = ConfirmAndPrintShippingLabel::class;
+
+    public function testSendTrackAndTrace()
+    {
+        $shipmentMock = $this->getFakeMock(Shipment::class, true);
+
+        $shipmentRepositoryMock = $this->getFakeMock(ShipmentRepository::class)->setMethods(['get'])->getMock();
+        $shipmentRepositoryMock->expects($this->once())->method('get')->willReturn($shipmentMock);
+
+        $trackMock = $this->getFakeMock(Track::class)->setMethods(['send'])->getMock();
+        $trackMock->expects($this->once())->method('send')->with($shipmentMock);
+
+        $instance = $this->getInstance([
+            'shipmentRepository' => $shipmentRepositoryMock,
+            'track' => $trackMock
+        ]);
+
+        $this->invoke('sendTrackAndTrace', $instance);
+    }
+}
