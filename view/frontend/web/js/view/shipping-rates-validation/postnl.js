f6e23bef9c7a7d9bc8f764df7ce38eff156f41b8
Michiel Gerritsen <michiel.gerritsen@tig.nl>
2017-11-07 15:34:24 +0100

    Sometimes the carriers list was not reloaded after entering the address. This caused to show the wrong price. (#121)

---
+++ view/frontend/web/js/view/shipping-rates-validation/postnl.js
@@ -0,0 +1,53 @@
+/**
+ *
+ *          ..::..
+ *     ..::::::::::::..
+ *   ::'''''':''::'''''::
+ *   ::..  ..:  :  ....::
+ *   ::::  :::  :  :   ::
+ *   ::::  :::  :  ''' ::
+ *   ::::..:::..::.....::
+ *     ''::::::::::::''
+ *          ''::''
+ *
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@tig.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@tig.nl for more information.
+ *
+ * @copyright   Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+/*browser:true*/
+/*global define*/
+define(
+    [
+        'uiComponent',
+        'Magento_Checkout/js/model/shipping-rates-validator',
+        'Magento_Checkout/js/model/shipping-rates-validation-rules',
+        '../../Models/shipping-rates-validator/postnl',
+        '../../Models/shipping-rates-validation-rules/postnl'
+    ],
+    function (
+        Component,
+        defaultShippingRatesValidator,
+        defaultShippingRatesValidationRules,
+        postnlShippingRatesValidator,
+        postnlShippingRatesValidationRules
+    ) {
+        "use strict";
+        defaultShippingRatesValidator.registerValidator('tig_postnl', postnlShippingRatesValidator);
+        defaultShippingRatesValidationRules.registerRules('tig_postnl', postnlShippingRatesValidationRules);
+        return Component;
+    }
+);
