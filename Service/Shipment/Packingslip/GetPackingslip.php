4de11d8f857a8d2b400b2af506badda9f31ff3a8
Mickey Beijer <mickey.beijer@tig.nl>
2018-02-21 18:24:17 +0100

    POSTNLM2-437 & POSTNLM2-438 : Added basics of merging first label with last packingslip page

--- Service/Shipment/Packingslip/GetPackingslip.php
+++ Service/Shipment/Packingslip/GetPackingslip.php
@@ -37,6 +37,10 @@ use TIG\PostNL\Service\Shipment\Label\Generate as LabelGenerate;
 use TIG\PostNL\Service\Shipment\Labelling\GetLabels;
 use TIG\PostNL\Service\Shipment\Packingslip\Generate as PackingslipGenerate;
 
+use TIG\PostNL\Service\Pdf\Fpdi;
+use TIG\PostNL\Service\Pdf\FpdiFactory;
+use TIG\PostNL\Service\Shipment\Label\File;
+
 class GetPackingslip
 {
     /**
@@ -65,6 +69,16 @@ class GetPackingslip
     private $packingslipGenerator;
 
     /**
+     * @var FpdiFactory
+     */
+    private $fpdiFactory;
+
+    /**
+     * @var File
+     */
+    private $file;
+
+    /**
      * GetPackingslip constructor.
      *
      * @param ShipmentRepositoryInterface $shipmentLabelRepository
@@ -72,19 +86,25 @@ class GetPackingslip
      * @param GetLabels                   $getLabels
      * @param LabelGenerate               $labelGenerator
      * @param Generate                    $packingslipGenerator
+     * @param FpdiFactory                 $fpdiFactory
+     * @param File                        $file
      */
     public function __construct(
         ShipmentRepositoryInterface $shipmentLabelRepository,
         PdfShipment $pdfShipment,
         GetLabels $getLabels,
         LabelGenerate $labelGenerator,
-        PackingslipGenerate $packingslipGenerator
+        PackingslipGenerate $packingslipGenerator,
+        FpdiFactory $fpdiFactory,
+        File $file
     ) {
         $this->shipmentRepository = $shipmentLabelRepository;
         $this->pdfShipment = $pdfShipment;
         $this->getLabels = $getLabels;
         $this->labelGenerator = $labelGenerator;
         $this->packingslipGenerator = $packingslipGenerator;
+        $this->fpdiFactory = $fpdiFactory;
+        $this->file = $file;
     }
 
     /**
@@ -102,26 +122,94 @@ class GetPackingslip
 
         $magentoShipment = $shipment->getShipment();
         $packingSlip = $this->pdfShipment->getPdf([$magentoShipment]);
-        $packingSlip = $packingSlip->render();
+        $currentYPosition = $this->pdfShipment->y;
+//        $packingSlip = $packingSlip->render();
 
-        $packingSlipPdf = $this->addLabels($shipmentId, $packingSlip);
+        $packingSlipPdf = $this->addLabels($shipmentId, $packingSlip, $currentYPosition);
 
         return $packingSlipPdf;
     }
 
     /**
-     * @param int $shipmentId
-     * @param string $packingslip
+     * @param int       $shipmentId
+     * @param \Zend_Pdf $packingslip
+     *
+     * @param           $currentYPosition
      *
      * @return string
      */
-    private function addLabels($shipmentId, $packingslip)
+    private function addLabels($shipmentId, $packingslip, $currentYPosition)
     {
         $labels = $this->getLabels->get($shipmentId);
-        $labelPdf = $this->labelGenerator->run($labels, true);
+        $pdfList = [];
+        $mergedLabel = $packingslip->render();
 
-        $packingslipPdf = $this->packingslipGenerator->run([$packingslip, $labelPdf]);
+        if ($currentYPosition > 400) {
+            $mergedLabel = $this->mergeFirstLabel(array_shift($labels), $packingslip);
+        }
+
+        $pdfList[] = $mergedLabel;
+
+        if (count($labels) > 0) {
+            $labelPdf = $this->labelGenerator->run($labels, true);
+            $pdfList[] = $labelPdf;
+        }
+
+        $packingslipPdf = $this->packingslipGenerator->run($pdfList);
 
         return $packingslipPdf;
+    }
+
+    /**
+     * @param \TIG\PostNL\Api\Data\ShipmentLabelInterface $label
+     * @param \Zend_Pdf                                   $packingslip
+     *
+     * @return string
+     */
+    private function mergeFirstLabel($label, $packingslip)
+    {
+        /** @var Fpdi $pdf */
+        $pdf = $this->fpdiFactory->create();
+
+        foreach ($packingslip->pages as $page) {
+            $packingslipPdfPage = new \Zend_Pdf();
+            $packingslipPdfPage->pages[] = clone $page;
+
+            $packingslipFile = $this->file->save($packingslipPdfPage->render());
+
+            $pdf->AddPage('P', 'A4');
+            $pdf->setSourceFile($packingslipFile);
+            $pageId = $pdf->importPage(1);
+            $pdf->useTemplate($pageId, 0, 0);
+        }
+
+        $labelFile = $this->file->save(base64_decode($label->getLabel()));
+        $pdf->Rotate(90);
+        $pdf->setSourceFile($labelFile);
+        $pageId = $pdf->importPage(1);
+        $pdf->useTemplate($pageId, $this->pix2pt(-1037), $this->pix2pt(413), $this->pix2pt(538));
+        $pdf->Rotate(0);
+
+        $this->file->cleanup();
+
+        return $pdf->Output('s');
+    }
+
+    /**
+     * Converts pixels to points. 3.8 pixels is 1 pt in pdfs.
+     *
+     * @param int $pixels
+     *
+     * @return int
+     */
+    private function pix2pt($pixels = 0)
+    {
+        $points = 0;
+
+        if ($pixels != 0) {
+            $points = round($pixels / 3.8, 1);
+        }
+
+        return $points;
     }
 }
