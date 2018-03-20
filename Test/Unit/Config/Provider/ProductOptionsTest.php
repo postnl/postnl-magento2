0303043016eec9e0dfa434dd0ed686cd1a57e2a2
Mickey Beijer <mickey.beijer@tig.nl>
2018-03-20 15:44:59 +0100

    POSTNLM2-483 : Added configuration options for alternative default product option

--- Test/Unit/Config/Provider/ProductOptionsTest.php
+++ Test/Unit/Config/Provider/ProductOptionsTest.php
@@ -56,6 +56,93 @@ class ProductOptionsTest extends AbstractConfigurationTest
         $this->assertEquals($value, $instance->getDefaultProductOption());
     }
 
+    /**
+     * @return array
+     */
+    public function getUseAlternativeDefaultProvider()
+    {
+        return [
+            'use alternative default option' => [1],
+            'do not alternative default option' => [0]
+        ];
+    }
+
+    /**
+     * @param $value
+     *
+     * @dataProvider getUseAlternativeDefaultProvider
+     */
+    public function testGetUseAlternativeDefault($value)
+    {
+        $instance = $this->getInstance();
+        $this->setXpath(ProductOptions::XPATH_USE_ALTERNATIVE_DEFAULT_OPTION, $value);
+        $this->assertEquals($value, $instance->getUseAlternativeDefault());
+    }
+
+    /**
+     * @return array
+     */
+    public function getAlternativeDefaultMaxAmountProvider()
+    {
+        return [
+            'alternative disabled' => [0, 500, 0],
+            'alternative enabled, no amount value' => [1, null, null],
+            'alternative enabled, zero amount value' => [1, 0, 0],
+            'alternative enabled, non-zero amount value' => [1, 300, 300]
+        ];
+    }
+
+    /**
+     * @param $enabled
+     * @param $value
+     * @param $expected
+     *
+     * @dataProvider getAlternativeDefaultMaxAmountProvider
+     */
+    public function testGetAlternativeDefaultMaxAmount($enabled, $value, $expected)
+    {
+        $instance = $this->getInstance();
+        $this->setXpathConsecutive(
+            [[ProductOptions::XPATH_USE_ALTERNATIVE_DEFAULT_OPTION], [ProductOptions::XPATH_ALTERNATIVE_DEFAULT_MAX_AMOUNT]],
+            [$enabled, $value]
+        );
+
+        $result = $instance->getAlternativeDefaultMaxAmount();
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @return array
+     */
+    public function getAlternativeDefaultProductOptionProvider()
+    {
+        return [
+            'alternative disabled' => [0, '3085', false],
+            'alternative enabled, no option set' => [1, null, null],
+            'alternative enabled, standard shipment' => [1, '3085', '3085'],
+            'alternative enabled, signature on delivery' => [1, '3189', '3189']
+        ];
+    }
+
+    /**
+     * @param $enabled
+     * @param $value
+     * @param $expected
+     *
+     * @dataProvider getAlternativeDefaultProductOptionProvider
+     */
+    public function testGetAlternativeDefaultProductOption($enabled, $value, $expected)
+    {
+        $instance = $this->getInstance();
+        $this->setXpathConsecutive(
+            [[ProductOptions::XPATH_USE_ALTERNATIVE_DEFAULT_OPTION], [ProductOptions::XPATH_ALTERNATIVE_DEFAULT_PRODUCT_OPTION]],
+            [$enabled, $value]
+        );
+
+        $result = $instance->getAlternativeDefaultProductOption();
+        $this->assertEquals($expected, $result);
+    }
+
     public function EveningOptionsProvider()
     {
         return [
