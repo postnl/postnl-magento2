6b670e10395b3779ee64d6037e3c369e8fdf6a38
Daan van den Bergh <daan.van.den.bergh@tig.nl>
2018-04-20 15:39:13 +0200

    Added shipping duration to config and translations

--- Config/Provider/Webshop.php
+++ Config/Provider/Webshop.php
@@ -33,9 +33,10 @@ namespace TIG\PostNL\Config\Provider;
 
 class Webshop extends AbstractConfigProvider
 {
-    const XPATH_WEBSHOP_LABEL_SIZE    = 'tig_postnl/webshop_printer/label_size';
-    const XPATH_WEBSHOP_CUTOFFTIME    = 'tig_postnl/webshop_shipping/cutoff_time';
-    const XPATH_WEBSHOP_SHIPMENTDAYS  = 'tig_postnl/webshop_shipping/shipment_days';
+    const XPATH_WEBSHOP_LABEL_SIZE        = 'tig_postnl/webshop_printer/label_size';
+    const XPATH_WEBSHOP_SHIPPING_DURATION = 'tig_postnl/webshop_shipping/shipping_duration';
+    const XPATH_WEBSHOP_CUTOFFTIME        = 'tig_postnl/webshop_shipping/cutoff_time';
+    const XPATH_WEBSHOP_SHIPMENTDAYS      = 'tig_postnl/webshop_shipping/shipment_days';
 
     const XPATH_TRACK_AND_TRACE_ENABLED       = 'tig_postnl/webshop_track_and_trace/email_enabled';
     const XPATH_TRACK_AND_TRACE_BCC_EMAIL     = 'tig_postnl/webshop_track_and_trace/email_bcc';
@@ -50,6 +51,14 @@ class Webshop extends AbstractConfigProvider
     public function getLabelSize()
     {
         return $this->getConfigFromXpath(self::XPATH_WEBSHOP_LABEL_SIZE);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getShippingDuration()
+    {
+        return $this->getConfigFromXpath(self::XPATH_WEBSHOP_SHIPPING_DURATION);
     }
 
     /**
