6cf05a2b45c5cc5bb33a5e5d7c310c24deb6b52b
Mickey Beijer <mickey.beijer@tig.nl>
2018-06-28 19:04:00 +0200

    POSTNLM2-552 : first setup submiting the toolbar data and selected orders to a different page

--- view/base/web/js/grid/toolbar.js
+++ view/base/web/js/grid/toolbar.js
@@ -31,15 +31,25 @@
 define([
     'jquery',
     'ko',
-    'Magento_Ui/js/grid/toolbar'
+    'underscore',
+    'Magento_Ui/js/grid/toolbar',
+    'mageUtils',
+    'mage/url'
 ], function (
     $,
     ko,
-    Toolbar
+    _,
+    Toolbar,
+    utils,
+    url
 ) {
     return Toolbar.extend({
         defaults : {
-            currentSelected : ko.observable('change_parcel')
+            currentSelected : ko.observable('change_parcel'),
+            selectProvider: 'ns = ${ $.ns }, index = ids',
+            modules: {
+                selections: '${ $.selectProvider }'
+            }
         },
 
         initObservable : function () {
@@ -63,6 +73,67 @@ define([
                 {name: $.mage.__('Change parcel'), value: 'change_parcel'},
                 {name: $.mage.__('Change product'), value: 'change_product'}
             ];
+        },
+
+        submit : function () {
+            /**
+             * In the frontend, url.build() will take care of everything (base url, form key etc.)
+             * and return the proper url. However, this doesn't seem to be the case in the backend,
+             * the base url needs to be set manually. TODO: Find a correct way to set the base url
+             */
+            url.setBaseUrl('http://postnl223.local/testadmin/');
+            var submitUrl = url.build('sales/order/massCancel/');
+
+
+            /**
+             * this will create a data json which will look similar to:
+             * {
+             *   change_parcel: "3", //data inserted in the new field
+             *   selected: ["1", "2"], //Selected orders from the grid. "selected" key could be "excluded", depending on your filters
+             *   filters: {....},
+             *   namespace: "..."
+             * }
+             */
+            var data = this.getSelectedItems();
+            data[this.currentSelected()] = jQuery('#'+this.currentSelected())[0].value;
+
+            console.log(submitUrl);
+            console.log(data);
+
+            /**
+             * utils.submit() will take care of everything; it will trigger a post request to the provided url,
+             * handle the form key if necessary, and redirect you to the url. All that it needs is a full url
+             * (so http://base.url/route/controller/action), and the post data.
+             */
+            utils.submit({
+                url: submitUrl,
+                data: data
+            });
+        },
+
+        /**
+         * Obtain and return the selected items from the grid
+         *
+         * @returns {*}
+         */
+        getSelectedItems : function () {
+            var provider = this.selections();
+            var selections = provider && provider.getSelections();
+            var itemsType = selections.excludeMode ? 'excluded' : 'selected';
+
+            var selectedItems = {};
+            selectedItems[itemsType] = selections[itemsType];
+
+            console.log(selections);
+
+            if (!selectedItems[itemsType].length) {
+                selectedItems[itemsType] = false;
+            }
+
+            // Params includes extra data like filters
+            _.extend(selectedItems, selections.params || {});
+
+            return selectedItems;
         }
     });
 });
