29b454ffdc2e404dc2aabe78a8166f532619050a
Rik Jonkmans <rik@tig.nl>
2018-08-10 10:17:15 +0200

    Fixed background in accountsettings tab configuration

--- Block/Adminhtml/Config/General/AccountSettings.php
+++ Block/Adminhtml/Config/General/AccountSettings.php
@@ -33,6 +33,7 @@ namespace TIG\PostNL\Block\Adminhtml\Config\General;
 
 use Magento\Framework\Data\Form\Element\Renderer\RendererInterface;
 use Magento\Framework\View\Element\Template;
+use TIG\PostNL\Config\Provider\AccountConfiguration;
 
 /**
  * Class AccountSettings
@@ -48,6 +49,20 @@ class AccountSettings extends Template implements RendererInterface
     protected $_template = 'TIG_PostNL::config/general/accountSettings.phtml';
 
     /**
+     * @var AccountConfiguration
+     */
+    private $accountConfig;
+
+    public function __construct(
+        Template\Context $context,
+        AccountConfiguration $accountConfiguration,
+        array $data = [])
+    {
+        parent::__construct($context, $data);
+        $this->accountConfig = $accountConfiguration;
+    }
+
+    /**
      * Render form element as HTML
      *
      * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
@@ -60,6 +75,26 @@ class AccountSettings extends Template implements RendererInterface
         $this->setElement($element);
 
         return $this->toHtml();
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getModusClass()
+    {
+        $modus   = $this->accountConfig->getModus();
+        $classes = [
+            '1' => 'modus_live',
+            '2' => 'modus_test',
+            '0' => 'modus_off'
+        ];
+
+        $className = 'modus_off';
+        if (array_key_exists($modus, $classes)) {
+            $className = $classes[$modus];
+        }
+
+        return $className;
     }
 
     /**
