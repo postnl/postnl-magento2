d2e2c4ba5c917b95a5cba1ce30faefcef21a2bba
Dennis van der Hammen <dennis.van.der.hammen@tig.nl>
2018-09-26 12:01:01 +0200

    A line exceeded more than 120 characters

--- Controller/Address/Postcode.php
+++ Controller/Address/Postcode.php
@@ -74,16 +74,20 @@ class Postcode extends Action
     {
         $params = $this->getRequest()->getParams();
         $params = $this->handler->convertRequest($params);
+
         if (!$params) {
             return $this->returnJson($this->getErrorResponse('error', __('Postcode request validation failed')));
         }
 
         $this->postcodeService->setRequestData($params);
         $result = $this->postcodeService->call();
+
         if ($result === false) {
-            return $this->returnJson($this->getErrorResponse(false, __('Zipcode and housenumber combination not found')));
+            return $this->returnJson(
+                $this->getErrorResponse(false, __('Zipcode and housenumber combination not found'))
+            );
         }
-        
+
         if ($result === 'error') {
             return $this->returnJson($this->getErrorResponse('error', __('Postcode response validation failed')));
         }
