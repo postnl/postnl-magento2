5b1bcbcf6d9975ed4425f9151a13d326b553af90
Rik Jonkmans <rik@tig.nl>
2018-10-02 10:23:56 +0200

    Try build without unit tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,13 +52,13 @@ module.exports = function (grunt) {
 
             unitTests: 'cd ' + magento2path + ' && vendor/phpunit/phpunit/phpunit -c "' + phpunitXmlPath + '"',
 
-            // integrationTests:
-            //     'cd ' + magento2path + 'dev/tests/integration &&' +
-            //     'php -ddisplay_errors=1 ../../../vendor/phpunit/phpunit/phpunit --testsuite "TIG PostNL Integration Tests"',
-
-            ciTests:
+            integrationTests:
                 'cd ' + magento2path + 'dev/tests/integration &&' +
-                'php -ddisplay_errors=1 ../../../vendor/phpunit/phpunit/phpunit',
+                'php -ddisplay_errors=1 ../../../vendor/phpunit/phpunit/phpunit --testsuite "TIG PostNL Integration Tests"',
+
+            // ciTests:
+            //     'cd ' + magento2path + 'dev/tests/integration &&' +
+            //     'php -ddisplay_errors=1 ../../../vendor/phpunit/phpunit/phpunit',
 
             phplint: 'if find . -name "*.php" ! -path "./vendor/*" -print0 | xargs -0 -n 1 -P 8 php -l | grep -v "No syntax errors detected"; then exit 1; fi',
 
