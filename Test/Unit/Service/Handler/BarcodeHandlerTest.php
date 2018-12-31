da66d73c44da4f25e8a7863bf0151876855aa52e
Rik Jonkmans <rik@tig.nl>
2018-12-31 11:14:37 +0100

    Fixed unit test

--- Test/Unit/Service/Handler/BarcodeHandlerTest.php
+++ Test/Unit/Service/Handler/BarcodeHandlerTest.php
@@ -34,6 +34,7 @@ namespace TIG\PostNL\Test\Unit\Service\Handler;
 use Magento\Framework\Phrase;
 use TIG\PostNL\Service\Handler\BarcodeHandler;
 use TIG\PostNL\Test\TestCase;
+use TIG\PostNL\Api\Data\ShipmentInterface;
 
 class BarcodeHandlerTest extends TestCase
 {
@@ -63,7 +64,7 @@ class BarcodeHandlerTest extends TestCase
         $barcodeMock = $this->getBarcodeMock($callReturnValue);
 
         $instance = $this->getInstance(['barcodeEndpoint' => $barcodeMock]);
-        $result = $this->invoke('generate', $instance);
+        $result = $this->invokeArgs('generate', [$this->getShippmentMock()], $instance);
 
         if ($result instanceof Phrase) {
             $result = $result->render();
@@ -79,13 +80,21 @@ class BarcodeHandlerTest extends TestCase
 
         try {
             $instance = $this->getInstance(['barcodeEndpoint' => $barcodeMock]);
-            $result = $this->invoke('generate', $instance);
+            $result = $this->invokeArgs('generate', [$this->getShippmentMock()], $instance);
             if ($result instanceof Phrase) {
                 $result->render();
             }
         } catch (\Magento\Framework\Exception\LocalizedException $exception) {
             $this->assertEquals($message, $exception->getMessage());
         }
+    }
+
+    protected function getShippmentMock()
+    {
+        $shipmentMock = $this->getFakeMock(ShipmentInterface::class)->getMock();
+        $shipmentMock->method('getProductCode')->willReturn('3085');
+
+        return $shipmentMock;
     }
 
     /**
