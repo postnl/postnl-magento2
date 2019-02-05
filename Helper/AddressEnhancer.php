56e4be166eeae9518c33323101353d8419bfe9cb
Dennis van der Hammen <dennis.van.der.hammen@tig.nl>
2019-02-05 12:07:19 +0100

    Github pull request #95 - Fix a few notices in the AddressEnhancer

--- Helper/AddressEnhancer.php
+++ Helper/AddressEnhancer.php
@@ -137,12 +137,19 @@ class AddressEnhancer
     // @codingStandardsIgnoreLine
     protected function parseResult($result, $address)
     {
+        if (!is_array($result)) {
+            return $address;
+        }
+        
         if ($result['street']) {
             $address['street'][0] = trim($result['street']);
         }
 
         if ($result['number']) {
             $address['housenumber'] = trim($result['number']);
+        }
+
+        if ($result['addition']) {
             $address['housenumberExtension'] = trim($result['addition']);
         }
 
