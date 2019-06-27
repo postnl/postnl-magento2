fb65d26517bb50490787ee99e9c1bfe02bc6f853
Daan van den Bergh <daan.van.den.bergh@tig.nl>
2019-06-27 10:41:38 +0200

    Modified Unit Tests to replicate new getSendDate()-functionality.

--- Test/Unit/Webservices/Endpoints/SentDateTest.php
+++ Test/Unit/Webservices/Endpoints/SentDateTest.php
@@ -29,6 +29,7 @@
  * @copyright   Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
  * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
  */
+
 namespace TIG\PostNL\Test\Unit\Webservices\Endpoints;
 
 use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
@@ -42,13 +43,13 @@ use TIG\PostNL\Webservices\Api\DeliveryDateFallback;
 class SentDateTest extends \TIG\PostNL\Test\TestCase
 {
     public $instanceClass = SentDate::class;
-
+    
     public function theParametersAreSetCorrectlyProvider()
     {
         /**
          * Today = 18-11-2016
          */
-
+        
         return [
             'Random address NL' => [
                 ['country' => 'NL', 'postcode' => '1014 BA', 'delivery_date' => '19-11-2016'],
@@ -62,13 +63,13 @@ class SentDateTest extends \TIG\PostNL\Test\TestCase
                 ['country' => 'DE', 'postcode' => '10179', 'delivery_date' => null],
                 ['country' => 'NL', 'postcode' => '2132WT', 'delivery_date' => '19-11-2016'],
             ],
-            'Reverse date' => [
+            'Reverse date'      => [
                 ['country' => 'NL', 'postcode' => '1014 BA', 'delivery_date' => '2016-11-19'],
                 ['country' => 'NL', 'postcode' => '1014BA', 'delivery_date' => '19-11-2016'],
             ],
         ];
     }
-
+    
     /**
      * @dataProvider theParametersAreSetCorrectlyProvider
      *
@@ -83,41 +84,52 @@ class SentDateTest extends \TIG\PostNL\Test\TestCase
         $address = $this->getObject(Address::class);
         $address->setCountryId($input['country']);
         $address->setPostcode($input['postcode']);
-
+        
         $shipmentMock = $this->getFakeMock(Shipment::class, true);
         $this->mockFunction($shipmentMock, 'getShippingAddress', $address);
-
+        
         $orderMock = $this->getMock(OrderInterface::class);
         $this->mockFunction($orderMock, 'getDeliveryDate', $input['delivery_date']);
-        $falbackMock  = $this->deliveryDateFallbackMock();
-
+        $fallbackMock = $this->deliveryDateFallbackMock();
+        $optionsMock  = $this->optionsFallbackMock();
+        
         $instance = $this->getInstance([
-            'dateFallback' => $falbackMock
+            'dateFallback'     => $fallbackMock,
+            'timeframeOptions' => $optionsMock
         ]);
         $instance->setParameters($address, 1, $orderMock);
-
+        
         $result = $this->getProperty('requestParams', $instance);
-
+        
         $this->assertEquals($expected['country'], $result['GetSentDate']['CountryCode']);
         $this->assertEquals($expected['postcode'], $result['GetSentDate']['PostalCode']);
         $this->assertEquals($expected['delivery_date'], $result['GetSentDate']['DeliveryDate']);
     }
-
+    
+    private function optionsFallbackMock()
+    {
+        $optionsMock     = $this->getFakeMock(\TIG\PostNL\Service\Timeframe\Options::class)->getMock();
+        $optionsExpected = $optionsMock->expects($this->any());
+        $optionsExpected->method('get');
+        $optionsExpected->willReturn(['Daytime']);
+        
+        return $optionsMock;
+    }
+    
     /**
      * @return \PHPUnit_Framework_MockObject_MockObject
      */
     private function deliveryDateFallbackMock()
     {
         $fallbackMock = $this->getFakeMock(DeliveryDateFallback::class)->getMock();
-        $getFalback = $fallbackMock->expects($this->any());
+        $getFalback   = $fallbackMock->expects($this->any());
         $getFalback->method('get');
         $getFalback->willReturn('19-11-2016');
-
+        
         $getFalback2 = $fallbackMock->expects($this->any());
         $getFalback2->method('getDate');
         $getFalback2->willReturn('19-11-2016');
-
-
+        
         return $fallbackMock;
     }
 }
