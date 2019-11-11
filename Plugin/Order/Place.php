da5fad1e512391b5ae81bb7255c076be9c6d550f
Kevin van Dijk <kevin.van.dijk@tig.nl>
2019-11-11 16:57:53 +0100

    POSTNLM2-892 update tig_postnl fields after refresh order grid when async is enabled

---
+++ Plugin/Order/Place.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ *
+ *          ..::..
+ *     ..::::::::::::..
+ *   ::'''''':''::'''''::
+ *   ::..  ..:  :  ....::
+ *   ::::  :::  :  :   ::
+ *   ::::  :::  :  ''' ::
+ *   ::::..:::..::.....::
+ *     ''::::::::::::''
+ *          ''::''
+ *
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@totalinternetgroup.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@tig.nl for more information.
+ *
+ * @copyright   Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+namespace TIG\PostNL\Plugin\Order;
+
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\Model\ResourceModel\Db\Context;
+use Magento\Sales\Model\ResourceModel\Grid;
+use Magento\Sales\Model\ResourceModel\Provider\NotSyncedDataProviderInterface;
+use TIG\PostNL\Model\OrderRepository;
+
+class Place extends Grid
+{
+    /**
+     * @var array
+     */
+    private $notSyncedIds;
+
+    /**
+     * @var NotSyncedDataProviderInterface
+     */
+    private $notSyncedDataProvider;
+
+    /**
+     * @var OrderRepository
+     */
+    private $orderRepository;
+
+    /**
+     * @var ResourceConnection
+     */
+    private $resourceConnection;
+
+    /**
+     * @param OrderRepository                     $orderRepository
+     * @param ResourceConnection                  $resourceConnection
+     * @param Context                             $context
+     * @param                                     $mainTableName
+     * @param                                     $gridTableName
+     * @param                                     $orderIdField
+     * @param array                               $joins
+     * @param array                               $columns
+     * @param null                                $connectionName
+     * @param NotSyncedDataProviderInterface|null $notSyncedDataProvider
+     */
+    public function __construct(
+        OrderRepository $orderRepository,
+        ResourceConnection $resourceConnection,
+        Context $context,
+        $mainTableName,
+        $gridTableName,
+        $orderIdField,
+        array $joins = [],
+        array $columns = [],
+        $connectionName = null,
+        NotSyncedDataProviderInterface $notSyncedDataProvider = null
+    ) {
+        parent::__construct(
+            $context,
+            $mainTableName,
+            $gridTableName,
+            $orderIdField,
+            $joins,
+            $columns,
+            $connectionName,
+            $notSyncedDataProvider
+        );
+
+        $this->orderRepository = $orderRepository;
+        $this->resourceConnection = $resourceConnection;
+        $this->notSyncedDataProvider = $notSyncedDataProvider;
+    }
+
+    public function beforeRefreshBySchedule()
+    {
+        $this->notSyncedIds = $this->notSyncedDataProvider->getIds($this->mainTableName, $this->gridTableName);
+    }
+
+    public function afterRefreshBySchedule()
+    {
+        foreach($this->notSyncedIds as $orderId) {
+            $postNLOrder = $this->orderRepository->getByOrderId($orderId);
+            $shipAt = $postNLOrder->getShipAt();
+            $productCode = $postNLOrder->getProductCode();
+            $confirmed = $postNLOrder->getConfirmed();
+            $connection = $this->resourceConnection->getConnection();
+
+            $binds = array(
+                'tig_postnl_ship_at'        => $shipAt,
+                'tig_postnl_product_code'   => $productCode,
+                'tig_postnl_confirmed'      => $confirmed,
+            );
+
+            $where = [$connection->quoteIdentifier('entity_id') . '=?' => $orderId];
+            $connection->update('sales_order_grid', $binds, $where);
+        }
+    }
+}
