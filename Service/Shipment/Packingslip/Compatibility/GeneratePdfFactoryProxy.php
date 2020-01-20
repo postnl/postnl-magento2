4b29fba55a17bf27588d8aea7a9c33cb0eb59824
Dennis van der Hammen <dennis.van.der.hammen@tig.nl>
2020-01-20 14:57:35 +0100

    POSTNLM2-894 - Xtento PR gerefactord en toegepast

---
+++ Service/Shipment/Packingslip/Compatibility/GeneratePdfFactoryProxy.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *
+ *          ..::..
+ *     ..::::::::::::..
+ *   ::'''''':''::'''''::
+ *   ::..  ..:  :  ....::
+ *   ::::  :::  :  :   ::
+ *   ::::  :::  :  ''' ::
+ *   ::::..:::..::.....::
+ *     ''::::::::::::''
+ *          ''::''
+ *
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@totalinternetgroup.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@tig.nl for more information.
+ *
+ * @copyright   Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+
+namespace TIG\PostNL\Service\Shipment\Packingslip\Compatibility;
+
+use Magento\Framework\ObjectManagerInterface;
+use Xtento\PdfCustomizer\Helper\Data;
+
+// @codingStandardsIgnoreFile
+class GeneratePdf
+{
+    /**
+     * @var ObjectManagerInterface
+     */
+    private $objectManager;
+
+    /**
+     * @var Data
+     */
+    private $subject;
+
+    /**
+     * DataHelperFactoryProxy constructor.
+     *
+     * @param ObjectManagerInterface $objectManager
+     */
+    public function __construct(ObjectManagerInterface $objectManager)
+    {
+        $this->objectManager = $objectManager;
+    }
+
+    /**
+     * @return mixed|Data
+     */
+    private function getSubject()
+    {
+        if (!$this->subject) {
+            $this->subject = $this->objectManager->get('\Xtento\PdfCustomizer\Helper\Data');
+        }
+
+        return $this->subject;
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return mixed
+     */
+    public function create(array $data = [])
+    {
+        return $this->getSubject()->create($data);
+    }
+}
