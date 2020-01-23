906cde599c6ff6918631e75aeb7bee6dd49988ce
Daan van den Bergh <daan.van.den.bergh@tig.nl>
2020-01-23 14:52:50 +0100

    POSTNLM2-922: Added install script to modify column 'condition_name' in 'tig_postnl_tablerate'.

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -82,13 +82,17 @@ class UpgradeSchema implements UpgradeSchemaInterface
         if (version_compare($context->getVersion(), '1.6.1', '<')) {
             $this->upgradeSchemas($this->upgradeSchemaObjects['v1.6.1'], $setup, $context);
         }
-    
+
         if (version_compare($context->getVersion(), '1.7.4', '<')) {
             $this->upgradeSchemas($this->upgradeSchemaObjects['v1.7.4'], $setup, $context);
         }
-        
+
         if (version_compare($context->getVersion(), '1.8.0', '<')) {
             $this->upgradeSchemas($this->upgradeSchemaObjects['v1.8.0'], $setup, $context);
+        }
+
+        if (version_compare($context->getVersion(), '1.8.2', '<')) {
+            $this->upgradeSchemas($this->upgradeSchemaObjects['v1.8.2'], $setup, $context);
         }
 
         $setup->endSetup();
