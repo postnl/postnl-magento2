e086f059849951e3cb5ea61cc01140762e3b2516
Mickey Beijer <mickey.beijer@tig.nl>
2020-07-16 16:10:33 +0200

    POSTNLM2-1035 : fixed codesniffer error

--- Helper/AbstractTracking.php
+++ Helper/AbstractTracking.php
@@ -176,7 +176,12 @@ abstract class AbstractTracking extends AbstractHelper
     {
         $order = $address->getOrder();
         $store = $order->getStore();
-        $storeLocale = $this->scopeConfig->getValue('general/locale/code', ScopeInterface::SCOPE_STORE, $store->getStoreId());
+        $storeLocale = $this->scopeConfig->getValue(
+            'general/locale/code',
+            ScopeInterface::SCOPE_STORE,
+            $store->getStoreId()
+        );
+
         $language = substr($storeLocale, 3, 2);
         $params = [
             'B' => $trackingNumber,
