d48863b2975ad3bcda3a2d8c86eea7733b8bdac6
Dennis van der Hammen <dennis.van.der.hammen@tig.nl>
2020-12-15 15:32:17 +0100

    Disable and re-enable xdebug when downgrading with pecl

--- Test/Script/ManageDependencies.sh
+++ Test/Script/ManageDependencies.sh
@@ -48,5 +48,10 @@ if [ $? != "0" ]; then
     npm install grunt-cli -g
 fi
 
+# Currently there is a bug in the pecl installer causing a segmentation fault when updating while
+# an extension is enabled: https://bugs.xdebug.org/view.php?id=1729
+sed -i 's/zend_/;zend_/g' $(php -i | grep 'xdebug.ini' |  sed 's/^.*=> //')
 # Pecl automatically installs xdebug 3.x, but Magento doesn't support it yet. Use this to downgrade.
 pecl install xdebug-2.9.8
+
+sed -i 's/;zend_/zend_/g' $(php -i | grep 'xdebug.ini' |  sed 's/^.*=> //')
