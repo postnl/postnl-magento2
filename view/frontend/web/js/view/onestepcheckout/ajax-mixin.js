276d6a9e904d66118d6b6024ef31a20025b9f33e
Dennis van der Hammen <dennis.van.der.hammen@tig.nl>
2021-01-11 15:34:44 +0100

    Check if housenumberElement exists in osc mixin

--- view/frontend/web/js/view/onestepcheckout/ajax-mixin.js
+++ view/frontend/web/js/view/onestepcheckout/ajax-mixin.js
@@ -46,6 +46,10 @@ define([
                 var housenumberElement = Registry.get('checkout.steps.shipping-step.shippingAddress.shipping-address-fieldset.postcode-field-group.field-group.housenumber');
                 var housenumberAdditionElement = Registry.get('checkout.steps.shipping-step.shippingAddress.shipping-address-fieldset.postcode-field-group.field-group.housenumber_addition');
 
+                if (!housenumberElement) {
+                    return result;
+                }
+
                 if (result['shippingAddress'] && result['shippingAddress']['street'] && result['shippingAddress']['street'].length > 0 && housenumberElement.value()) {
                     result['shippingAddress']['street'][1] = housenumberElement.value();
 
