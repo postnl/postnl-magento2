27ac16eadcbf1bb62ec84ca3a250b3fa4d6b68aa
Kevin van Dijk <kevin.van.dijk@tig.nl>
2021-07-23 11:15:23 +0200

    check if price is set

--- Controller/DeliveryOptions/Timeframes.php
+++ Controller/DeliveryOptions/Timeframes.php
@@ -143,7 +143,7 @@ class Timeframes extends AbstractDeliveryOptions
         $quote = $this->checkoutSession->getQuote();
         $cartItems = $quote->getAllItems();
 
-        if ($this->letterboxPackage->isLetterboxPackage($cartItems, false) && $params['address']['country'] == 'NL') {
+        if ($this->letterboxPackage->isLetterboxPackage($cartItems, false) && $params['address']['country'] == 'NL' && isset($price['price'])) {
             return $this->jsonResponse($this->getLetterboxPackageResponse($price['price']));
         }
 
