f20a68b4574bbef03ff6218c951e5f164ed21b01
Lucas Hoeffnagel <lucas.hoeffnagel@tig.nl>
2021-08-24 14:52:36 +0200

    POSTNLM2-1185 & POSTNLM2-1186 - fixed handling a false rate price for table rates, fixed an issue where the condition value was missing in the rate request

--- Service/Carrier/QuoteToRateRequest.php
+++ Service/Carrier/QuoteToRateRequest.php
@@ -79,6 +79,7 @@ class QuoteToRateRequest
         $rateRequest->setDestRegionId($address->getRegionId());
         $rateRequest->setOrderSubtotal($this->quote->getSubtotal());
         $rateRequest->setFreeShipping((bool)$address->getFreeShipping());
+        $rateRequest->setPackageValueWithDiscount($address->getBaseSubtotalWithDiscount());
 
         return $rateRequest;
     }
