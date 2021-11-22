26fe585af6bea9f4b5180cd75f5b386c159bce97
Kevin van Dijk <kevin.van.dijk@tig.nl>
2021-11-22 16:33:01 +0100

    POSTNLM2-1221 Validate API Credentials

---
+++ Block/Adminhtml/Config/Validate/ApiCredentials.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ *
+ *          ..::..
+ *     ..::::::::::::..
+ *   ::'''''':''::'''''::
+ *   ::..  ..:  :  ....::
+ *   ::::  :::  :  :   ::
+ *   ::::  :::  :  ''' ::
+ *   ::::..:::..::.....::
+ *     ''::::::::::::''
+ *          ''::''
+ *
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to support@tig.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact support@tig.nl for more information.
+ *
+ * @copyright   Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+namespace TIG\PostNL\Block\Adminhtml\Config\Validate;
+
+use Magento\Backend\Block\Widget\Button;
+use Magento\Config\Block\System\Config\Form\Field;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+use Magento\Framework\Exception\LocalizedException;
+
+class ApiCredentials extends Field
+{
+    /** @var string */
+    //@codingStandardsIgnoreLine
+    protected $_template = 'TIG_PostNL::config/validate/apiCredentials.phtml';
+
+    /**
+     * Remove scope label
+     *
+     * @param  AbstractElement $element
+     * @return string
+     */
+    public function render(AbstractElement $element)
+    {
+        $element->unsScope();
+        $element->unsCanUseWebsiteValue();
+        $element->unsCanUseDefaultValue();
+
+        return parent::render($element);
+    }
+
+    /**
+     * Return element html
+     *
+     * @param  AbstractElement $element
+     * @return string
+     */
+    //@codingStandardsIgnoreLine
+    protected function _getElementHtml(AbstractElement $element)
+    {
+        return $this->_toHtml();
+    }
+
+    /**
+     * Return ajax url for collect button
+     *
+     * @return string
+     */
+    public function getAjaxUrl()
+    {
+        return $this->getUrl('postnl/config_validate/apiCredentials');
+    }
+
+    /**
+     * Generate collect button html
+     *
+     * @return string
+     *
+     * @throws LocalizedException
+     */
+    public function getButtonHtml()
+    {
+        $layout = $this->getLayout();
+        $buttonBlock = $layout->createBlock(Button::class);
+        $buttonBlock->setData(
+            [
+                'id' => 'validate_api_credentials',
+                //@codingStandardsIgnoreLine
+                'label' => __('Validate API Credentials'),
+            ]
+        );
+
+        return $buttonBlock->toHtml();
+    }
+}
