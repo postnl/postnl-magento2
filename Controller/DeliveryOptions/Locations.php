8d52af519f604f65500d26ed8454632e8a463969
Kevin van Dijk <kevin.van.dijk@tig.nl>
2022-01-06 15:25:13 +0100

    POSTNLM2-1096 Disable pickup locations for Letterbox packages.

--- Controller/DeliveryOptions/Locations.php
+++ Controller/DeliveryOptions/Locations.php
@@ -36,6 +36,7 @@ use TIG\PostNL\Model\OrderRepository;
 use TIG\PostNL\Helper\AddressEnhancer;
 use TIG\PostNL\Service\Carrier\Price\Calculator;
 use TIG\PostNL\Service\Carrier\QuoteToRateRequest;
+use TIG\PostNL\Service\Shipping\LetterboxPackage;
 use TIG\PostNL\Webservices\Endpoints\Locations as LocationsEndpoint;
 use TIG\PostNL\Webservices\Endpoints\DeliveryDate;
 use Magento\Framework\App\Action\Context;
@@ -55,6 +56,11 @@ class Locations extends AbstractDeliveryOptions
     private $priceCalculator;
 
     /**
+     * @var LetterboxPackage
+     */
+    private $letterboxPackage;
+
+    /**
      * @param Context            $context
      * @param OrderRepository    $orderRepository
      * @param Session            $checkoutSession
@@ -64,6 +70,7 @@ class Locations extends AbstractDeliveryOptions
      * @param DeliveryDate       $deliveryDate
      * @param Calculator         $priceCalculator
      * @param ShippingDuration   $shippingDuration
+     * @param LetterboxPackage   $letterboxPackage
      */
     public function __construct(
         Context $context,
@@ -74,11 +81,13 @@ class Locations extends AbstractDeliveryOptions
         LocationsEndpoint $locations,
         DeliveryDate $deliveryDate,
         Calculator $priceCalculator,
-        ShippingDuration $shippingDuration
+        ShippingDuration $shippingDuration,
+        LetterboxPackage $letterboxPackage
     ) {
         $this->addressEnhancer   = $addressEnhancer;
         $this->locationsEndpoint = $locations;
         $this->priceCalculator   = $priceCalculator;
+        $this->letterboxPackage = $letterboxPackage;
 
         parent::__construct(
             $context,
@@ -95,6 +104,13 @@ class Locations extends AbstractDeliveryOptions
      */
     public function execute()
     {
+        $products = $this->checkoutSession->getQuote()->getAllItems();
+        if ($this->letterboxPackage->isLetterboxPackage($products, false)) {
+            return $this->jsonResponse([
+                'error' => __('Pickup locations are disabled for Letterbox packages.')
+            ]);
+        }
+
         $params = $this->getRequest()->getParams();
         if (!isset($params['address']) || !is_array($params['address'])) {
             return $this->jsonResponse(__('No Address data found.'));
