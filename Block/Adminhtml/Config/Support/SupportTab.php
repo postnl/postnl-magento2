b4b63dab06fd9fec7260ba32fb513913234e47f5
Robert <robert.grundeken@tig.nl>
2023-01-23 10:17:17 +0100

    version bump

--- Block/Adminhtml/Config/Support/SupportTab.php
+++ Block/Adminhtml/Config/Support/SupportTab.php
@@ -38,7 +38,7 @@ use TIG\PostNL\Config\Provider\PostNLConfiguration;
 
 class SupportTab extends Template implements RendererInterface
 {
-    const POSTNL_VERSION = '1.12.6.1';
+    const POSTNL_VERSION = '1.12.7';
 
     const XPATH_SUPPORTED_MAGENTO_VERSION = 'tig_postnl/supported_magento_version';
 
