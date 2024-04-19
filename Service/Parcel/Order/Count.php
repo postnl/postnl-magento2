2785d9845bee4eaa29a19e138bb7cbc06eee4bfa
Anatol Alfer <anatoly@h1.nl>
2024-04-19 16:24:02 +0300

    POSTNL-130 - Allow parcels only for domestic countries

--- Service/Parcel/Order/Count.php
+++ Service/Parcel/Order/Count.php
@@ -2,19 +2,25 @@
 
 namespace TIG\PostNL\Service\Parcel\Order;
 
-use \TIG\PostNL\Service\Parcel\CountAbstract;
-use \Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Model\Order;
+use TIG\PostNL\Service\Parcel\CountAbstract;
 
 class Count extends CountAbstract
 {
     /**
-     * @param OrderInterface $order
+     * @param Order $order
      *
-     * @return int|\Magento\Framework\Api\AttributeInterface|null
+     * @return int|float
      */
-    public function get(OrderInterface $order)
+    public function get(Order $order)
     {
         $items = $order->getItems();
+        $shipping = $order->getShippingAddress();
+        $countryId = $shipping ? $shipping->getCountryId() : '';
+        // Only domestic shipments allowed for several packages
+        if ($countryId !== 'NL' && $countryId !== 'BE') {
+            return 1;
+        }
         return $this->calculate($order->getWeight(), $items);
     }
 }
