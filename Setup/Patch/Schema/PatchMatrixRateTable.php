f866fde5c403b8af1d45425dea2bb6670e144053
Liudmyla <nnn-lyusik@gmail.com>
2025-07-30 15:43:29 +0200

    POSTNL-312 - fix SQL error "key was too long; max key length is 3072 bytes" on tig_postnl_matrixrate table

--- Setup/Patch/Schema/PatchMatrixRateTable.php
+++ Setup/Patch/Schema/PatchMatrixRateTable.php
@@ -32,8 +32,7 @@ class PatchMatrixRateTable implements SchemaPatchInterface
     {
         return [];
     }
-
-
+    
     public function apply()
     {
         $this->moduleDataSetup->startSetup();
