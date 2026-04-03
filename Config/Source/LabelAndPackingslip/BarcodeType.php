e359a3cd7706c76960a706cd4444834cd4c83ca8
Andrii Adamchuk <andrii@h1.nl>
2026-04-03 04:39:45 +0300

    POSTNL-310 barcode library replacement

--- Config/Source/LabelAndPackingslip/BarcodeType.php
+++ Config/Source/LabelAndPackingslip/BarcodeType.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace TIG\PostNL\Config\Source\LabelAndPackingslip;
 
@@ -10,20 +11,14 @@ class BarcodeType implements OptionSourceInterface
      * These are the only Zend Barcode types
      * @see \Zend_Barcode_Object_ObjectAbstract
      * which support full length increment ID's.
-     *
-     * @return array
      */
-    public function toOptionArray()
+    public function toOptionArray(): array
     {
-        // @codingStandardsIgnoreStart
-        $options = [
-            ['value' => 'code25', 'label' => __('Code 25')],
-            ['value' => 'code39', 'label' => __('Code 39')],
-            ['value' => 'code128', 'label' => __('Code 128')],
-            ['value' => 'royalmail', 'label' => __('Royalmail')],
+        return [
+            ['value' => 'I25', 'label' => __('Code 25')],
+            ['value' => 'C39', 'label' => __('Code 39')],
+            ['value' => 'C128', 'label' => __('Code 128')],
+            ['value' => 'RMS4CC', 'label' => __('Royalmail')],
         ];
-        // @codingStandardsIgnoreEnd
-
-        return $options;
     }
 }
