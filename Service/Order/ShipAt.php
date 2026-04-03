c57bf2b1e159f42c859f576bc05844362c9ce417
Andrii Adamchuk <andrii@h1.nl>
2026-04-03 15:11:37 +0300

    POSTNL-422 fix delivery date

--- Service/Order/ShipAt.php
+++ Service/Order/ShipAt.php
@@ -1,40 +1,43 @@
 <?php
+declare(strict_types=1);
 
 namespace TIG\PostNL\Service\Order;
 
+use Exception;
 use TIG\PostNL\Api\Data\OrderInterface;
+use TIG\PostNL\Config\Source\General\PickupCountries;
 use TIG\PostNL\Service\Wrapper\QuoteInterface;
+use TIG\PostNL\Webservices\Api\DeliveryDateFallback;
 use TIG\PostNL\Webservices\Endpoints\SentDate;
+use function in_array;
 
 class ShipAt
 {
-    /**
-     * @var QuoteInterface
-     */
-    private $quote;
+    private QuoteInterface $quote;
 
-    /**
-     * @var SentDate
-     */
-    private $sentDate;
+    private SentDate $sentDate;
 
-    /**
-     * @param QuoteInterface $quote
-     * @param SentDate       $endpoint
-     */
+    private DeliveryDateFallback $deliveryDateFallback;
+
+    private array $internationalPickupCountries = [
+        PickupCountries::COUNTRY_DE,
+        PickupCountries::COUNTRY_FR,
+        PickupCountries::COUNTRY_DK,
+    ];
+
     public function __construct(
         QuoteInterface $quote,
-        SentDate $endpoint
+        SentDate $endpoint,
+        DeliveryDateFallback $deliveryDateFallback
     ) {
-        $this->quote    = $quote;
+        $this->quote = $quote;
         $this->sentDate = $endpoint;
+        $this->deliveryDateFallback = $deliveryDateFallback;
     }
 
     /**
      * GetSentDate calls could break during holidays, but this variable is only used to inform merchants.
      * It shouldn't break the shipping flow. This is the reason why $sentDate is set to null on failure. #POSTNLM2-1012
-     *
-     * @param OrderInterface $order
      *
      * @return OrderInterface|null
      */
@@ -50,12 +53,22 @@ class ShipAt
             return null;
         }
 
+        // The SentDate API only supports NL and BE addresses. For international pickup
+        // countries (DE, FR, DK) it falls back to an NL postal code (2521CA), which
+        // produces a shipping date unrelated to the actual handover day. Instead, we
+        // derive the ship-at date directly: the first valid shipping day from today.
+        if (in_array($address->getCountryId(), $this->internationalPickupCountries, true)) {
+            $order->setShipAt($this->deliveryDateFallback->get());
+
+            return $order;
+        }
+
         $storeId = $this->quote->getStoreId();
         $this->sentDate->updateParameters($address, $storeId, $order);
 
         try {
             $sentDate = $this->sentDate->call();
-        } catch (\Exception $exception) {
+        } catch (Exception) {
             $sentDate = null;
         }
 
--- Service/Shipping/DeliveryDate.php
+++ Service/Shipping/DeliveryDate.php
@@ -2,20 +2,30 @@
 
 namespace TIG\PostNL\Service\Shipping;
 
+use DateInterval;
+use DateTime;
 use Magento\Checkout\Model\Session;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
-use Magento\Framework\Webapi\Exception;
 use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
+use Magento\Framework\Webapi\Exception;
+use TIG\PostNL\Config\Provider\Webshop;
 use TIG\PostNL\Config\Source\General\PickupCountries;
 use TIG\PostNL\Service\Quote\ShippingDuration;
 use TIG\PostNL\Service\Timeframe\Filters\DaysSkipInterface;
+use TIG\PostNL\Service\Timeframe\IsPastCutOff;
 use TIG\PostNL\Webservices\Endpoints\DeliveryDate as DeliveryDateEndpoint;
+use function __;
+use function explode;
+use function in_array;
+use function is_object;
 
 class DeliveryDate
 {
     private DeliveryDateEndpoint $deliveryEndpoint;
+
     private Session $checkoutSession;
+
     private ShippingDuration $shippingDuration;
 
     /**
@@ -28,19 +38,28 @@ class DeliveryDate
         PickupCountries::COUNTRY_FR => 4,
         PickupCountries::COUNTRY_DK => 3,
     ];
+
     private TimezoneInterface $timezone;
+
+    private IsPastCutOff $isPastCutOff;
+
+    private Webshop $webshop;
 
     public function __construct(
         Session $checkoutSession,
         DeliveryDateEndpoint $deliveryDateEndpoint,
         ShippingDuration $shippingDuration,
         TimezoneInterface $timezone,
+        IsPastCutOff $isPastCutOff,
+        Webshop $webshop,
         array $daysFilter = []
     ) {
         $this->checkoutSession = $checkoutSession;
         $this->deliveryEndpoint = $deliveryDateEndpoint;
         $this->shippingDuration = $shippingDuration;
         $this->timezone = $timezone;
+        $this->isPastCutOff = $isPastCutOff;
+        $this->webshop = $webshop;
         $this->daysFilter = $daysFilter;
     }
 
@@ -65,34 +84,18 @@ class DeliveryDate
         $response = $this->deliveryEndpoint->call();
 
         if (!is_object($response) || !isset($response->DeliveryDate)) {
-            return __('Invalid GetDeliveryDate response: %1', var_export($response, true));
+            return __('Invalid GetDeliveryDate response: %1', var_export($response, true))->render();
         }
 
         $this->checkoutSession->setPostNLDeliveryDate($response->DeliveryDate);
+
         return $response->DeliveryDate;
-    }
-
-    private function checkOtherCountries(array $address): ?string
-    {
-        if (!isset($this->approximateDeliveryTime[$address['country']])) {
-            return null;
-        }
-        $date = $this->timezone->date();
-        $shippingDuration = $this->shippingDuration->get();
-        if ($shippingDuration > 0) {
-            $shippingDuration --;
-        }
-        $shippingDuration += $this->approximateDeliveryTime[$address['country']];
-
-        $interval = new \DateInterval('P'.(int)$shippingDuration.'D');
-        $date->add($interval);
-        return $date->format('d-m-Y');
     }
 
     public function advanceDisabledPickupDate(string $initialDate): string
     {
         try {
-            $day = new \DateTime($initialDate);
+            $day = new DateTime($initialDate);
             $updated = true;
             $iteration = 0;
             // Need to repeat days validations in case any day change is affected.
@@ -107,10 +110,59 @@ class DeliveryDate
                     break;
                 }
             }
+
             return $day->format('d-m-Y');
         } catch (\Exception $e) {
             return $initialDate;
         }
+    }
+
+    private function checkOtherCountries(array $address): ?string
+    {
+        if (!isset($this->approximateDeliveryTime[$address['country']])) {
+            return null;
+        }
+
+        $shippingDays = explode(',', $this->webshop->getShipmentDays());
+        $date = $this->timezone->date();
+
+        // If today is not a valid shipping day or the cutoff has already passed,
+        // advance to the next day before searching for the first valid shipping day.
+        $todayDayNumber = $this->normaliseToConfigDayNumber($date->format('N'));
+        if (!in_array($todayDayNumber, $shippingDays) || $this->isPastCutOff->calculate()) {
+            $date->modify('+1 day');
+        }
+
+        // Advance until we land on a configured shipping day (cap at 14 to prevent loops).
+        $i = 0;
+        while (!in_array($this->normaliseToConfigDayNumber($date->format('N')), $shippingDays) && $i < 14) {
+            $date->modify('+1 day');
+            $i++;
+        }
+
+        // Add any extra warehouse-processing days on top of the first valid shipping day.
+        $shippingDuration = $this->shippingDuration->get();
+        if ($shippingDuration > 0) {
+            $date->modify('+' . (int) $shippingDuration . ' days');
+        }
+
+        // Add the international transit days for this country.
+        $date->add(new DateInterval('P' . $this->approximateDeliveryTime[$address['country']] . 'D'));
+
+        return $date->format('d-m-Y');
+    }
+
+    /**
+     * Convert date('N') output (1=Mon … 7=Sun) to the day-number format used in
+     * the shipment-days configuration, where Sunday is stored as '0'.
+     */
+    private function normaliseToConfigDayNumber(string $isoWeekday): string
+    {
+        if ($isoWeekday === '7') {
+            return '0';
+        }
+
+        return $isoWeekday;
     }
 
 }
